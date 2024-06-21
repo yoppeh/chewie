d6768bb627644b9c2455e4ecc9edc907d8b63b03
Warren Mann <warren@nonvol.io>
2024-06-21 17:17:34 -0500

    Write debug output to stderr

--- chewie.h
+++ chewie.h
@@ -18,7 +18,7 @@
 #include <stdio.h>
 extern int indent_level;
 extern char *indent_string;
-#define debug(...) do { printf("%.*s%s  %d  ", indent_level, indent_string, __FILE__, __LINE__); printf(__VA_ARGS__); } while(0)
+#define debug(...) do { fprintf(stderr, "%.*s%s  %d  ", indent_level, indent_string, __FILE__, __LINE__); fprintf(stderr, __VA_ARGS__); } while(0)
 #define debug_indent_inc() do { indent_level += 4; if (indent_level > 20) indent_level = 20; } while(0)
 #define debug_indent_dec() do { indent_level -= 4; if (indent_level < 0) indent_level = 0; } while(0)
 #define debug_enter() do { debug("%s()\n", __PRETTY_FUNCTION__); indent_level += 4; } while(0)
